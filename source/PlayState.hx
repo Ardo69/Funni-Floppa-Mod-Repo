06165b01e6909c4d63a3621ae07325a35c302b06
ArdaBeyNotFound <98959759+SnakDev@users.noreply.github.com>
2022-06-14 17:48:59 +0300

    trrttrtr

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2735,9 +2735,7 @@ class PlayState extends MusicBeatState
 		{
 			return;
 		} else {
-			var achieve:String = checkForAchievement(['extras_passed', 'week2_nomiss', 'week3_nomiss', 'week4_nomiss',
-				'week5_nomiss', 'week6_nomiss', 'week7_nomiss', 'ur_bad',
-				'ur_good', 'hype', 'two_keys', 'toastie', 'debugger']);
+			var achieve:String = checkForAchievement(['extras_passed', 'extra_passed']);
 
 			if (achieve != null)
 			{
