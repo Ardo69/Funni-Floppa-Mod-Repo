7856060e8c3baeb415ac68fdf1fad7dd94d13e78
ArdaBeyNotFound <98959759+ArdoDev-stupid@users.noreply.github.com>
2022-07-16 09:40:54 +0300

    push harder plank

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -760,7 +760,7 @@ class PlayState extends MusicBeatState
 		iconP2.alpha = ClientPrefs.healthBarAlpha;
 		add(iconP2);
 		reloadHealthBarColors();
-		scoreTxt = new FlxText(0, healthBarBG.y + 36, FlxG.width, "", 20);
+		scoreTxt = new FlxText(215, healthBarBG.y + 36, FlxG.width, "", 20);
 		scoreTxt.setFormat(Paths.font("sanspro-regular.ttf"), 20, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		scoreTxt.scrollFactor.set();
 		scoreTxt.borderSize = 1.25;
