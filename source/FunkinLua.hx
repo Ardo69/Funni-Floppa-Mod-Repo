82ae65fd1821a7d715ecf58af783c43a86bc830e
ArdaBeyNotFound <98959759+ArdoDev-stupid@users.noreply.github.com>
2022-07-18 14:31:44 +0300

    fix 2

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -2233,6 +2233,8 @@ class FunkinLua {
 			return str.endsWith(end);
 		});
 
+		//SHADER SHIT
+		
 		Lua_helper.add_callback(lua, "addChromaticAbberationEffect", function(camera:String,chromeOffset:Float = 0.005) {
 			
 			PlayState.instance.addShaderToCamera(camera, new ChromaticAberrationEffect(chromeOffset));
@@ -2254,53 +2256,11 @@ class FunkinLua {
 			PlayState.instance.addShaderToCamera(camera, new TiltshiftEffect(blurAmount,center));
 			
 		});
-		Lua_helper.add_callback(lua, "addOldVCREffect", function(camera:String,glitchFactor:Float = 0.0,distortion:Bool=true,perspectiveOn:Bool=true,vignetteMoving:Bool=true) {
+		Lua_helper.add_callback(lua, "addVCREffect", function(camera:String,glitchFactor:Float = 0.0,distortion:Bool=true,perspectiveOn:Bool=true,vignetteMoving:Bool=true) {
 			
 			PlayState.instance.addShaderToCamera(camera, new VCRDistortionEffect(glitchFactor,distortion,perspectiveOn,vignetteMoving));
 			
 		});
-		Lua_helper.add_callback(lua, "addVCREffect", function(camera:String) { //for dem funkies
-			
-			PlayState.instance.addShaderToCamera(camera, new VCRDistortionShader());
-			
-		});
-
-		Lua_helper.add_callback(lua, "createShaders", function(shaderName:String, ?optimize:Bool = false)
-		{
-			var shader = new DynamicShaderHandler(shaderName, optimize);
-		
-			return shaderName;
-		});
-		/*
-		Lua_helper.add_callback(lua, "modifyShaderProperty", function(shaderName:String, propertyName:String, value:Dynamic)
-		{
-			//var handler:DynamicShaderHandler = PlayState.instance.luaShaders.get(shaderName);
-			//trace(Reflect.getProperty(handler.shader.data, propertyName));
-			//Reflect.setProperty(Reflect.getProperty(handler.shader.data, propertyName), 'value', value);
-			handler.modifyShaderProperty(propertyName, value);
-		});
-		// shader set
-		*/
-		/*Lua_helper.add_callback(lua, "setShadersToCamera", function(shaderName:Array<String>, cameraName:String)
-		{
-			
-			var shaderArray = new Array<BitmapFilter>();
-		
-			for (i in shaderName)
-			{
-				shaderArray.push(new ShaderFilter(PlayState.instance.luaShaders[i].shader));
-			}
-		
-			cameraFromString(cameraName).setFilters(shaderArray);
-		});*/
-		
-		// shader clear
-		
-		Lua_helper.add_callback(lua, "clearShadersFromCamera", function(cameraName)
-		{
-			cameraFromString(cameraName).setFilters([]);
-		});	
-					
 		Lua_helper.add_callback(lua, "addGlitchEffect", function(camera:String,waveSpeed:Float = 0.1,waveFrq:Float = 0.1,waveAmp:Float = 0.1) {
 			
 			PlayState.instance.addShaderToCamera(camera, new GlitchEffect(waveSpeed,waveFrq,waveAmp));
