fdf8c9cf1c725a4d66a79f26643620ac9eaea7c4
ArdaBeyNotFound <98959759+ArdoDev-stupid@users.noreply.github.com>
2023-01-16 19:20:58 +0300

    ok enough experimenting

--- source/ShopState.hx
+++ source/ShopState.hx
@@ -10,18 +10,38 @@ import flixel.text.FlxText;
 import flixel.util.FlxColor;
 import lime.utils.Assets;
 import flixel.system.FlxSound;
+import flixel.FlxSprite;
+import flixel.animation.FlxBaseAnimation;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.system.FlxBGSprite;
 
 using StringTools;
 
 class ShopState extends MusicBeatState
 {
+
+	//public var bigBalls:FlxText;
+	var hand:FlxSprite;
+
 	override public function create(){
 		FlxG.sound.playMusic(Paths.music('shop'), 0); // bwomp
 		FlxG.sound.music.fadeIn(0.5, 0, 0.65);
 		Conductor.changeBPM(96);
 
-		var midGround:BGSprite = new BGSprite('stages/house');
-		add(midGround);
+		/*bigBalls = new FlxText(0, 0, FlxG.width, "I have 2 big balls and cancer xD", 55, true);
+		bigBalls.setFormat(Paths.font("sanspro-bold.ttf"), 32, FlxColor.WHITE, CENTER, OUTLINE, FlxColor.RED);
+		bigBalls.y = FlxG.height * 0.89;
+		add(bigBalls);*/
+
+		hand = new FlxSprite(0, 0);
+		hand.frames = Paths.getSparrowAtlas('hand');
+
+		hand.antialiasing = ClientPrefs.globalAntialiasing;
+		hand.animation.addByPrefix('pointOut', 'hand instance', 24, false);
+		hand.animation.play('pointOut');
+		hand.screenCenter();
+		hand.updateHitbox();
+		add(hand);
 	}
 
 	override function update(elapsed:Float)
@@ -34,8 +54,8 @@ class ShopState extends MusicBeatState
 			FlxG.switchState(new FreeplayState());
 		}
 
-		if (controls.UI_UP || controls.UI_LEFT || controls.UI_RIGHT || controls.UI_RIGHT){
-			FlxG.switchState(new JokeSecretState());
+		if (controls.NOTE_UP || controls.NOTE_LEFT || controls.NOTE_RIGHT || controls.NOTE_RIGHT){
+		    hand.x += 100;
 		}
 
 		super.update(elapsed);
